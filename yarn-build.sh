e12a0a9dab6dbf9325337a56868c7962af33335c
loading0 <307958035@qq.com>
2022-07-27 17:03:14 +0800

    add npm build shell

---
+++ yarn-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+# 构建目录-Jenkinsfile定义的环境变量 ${TARGET_HOME_DIR}
+build_dir=${TARGET_HOME_DIR}
+ 
+echo "构建目录-Jenkinsfile定义的环境变量 'build_dir' >>> ${build_dir}"
+ 
+echo -e "\n--------------- npm开始构建打包 start ---------------\n"
+ 
+# 删除以前构建的dist文件
+sh "rm -rf ${build_dir}/dist"
+echo "删除以前构建的dist文件目录 >>> ${build_dir}/dist"
+ 
+# 进入工程打包构建的目录，构建
+cd ${build_dir} && npm run build
+ 
+echo -e "\n--------------- npm开始构建打包 end ---------------\n"
