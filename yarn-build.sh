eacd12f4ab145cb2e2036287ac383a2a4e0f1435
loading0 <307958035@qq.com>
2022-08-04 15:48:41 +0800

    debug  yarn-build.sh script

--- yarn-build.sh
+++ yarn-build.sh
@@ -8,12 +8,13 @@ echo "构建目录-Jenkinsfile定义的环境变量 'build_dir' >>> ${build_dir}
 echo -e "\n--------------- npm开始构建打包 start ---------------\n"
  
 # 删除以前构建的dist文件
-sh "rm -rf ${build_dir}/dist"
-sh "rm -rf ${build_dir}/node_modules"
+# sh "rm -rf ${build_dir}/dist"
+# sh "rm -rf ${build_dir}/node_modules"
+rm -rf ${build_dir}/node_modules/assert
 echo "删除以前构建的dist文件目录 >>> ${build_dir}/dist"
  
 # 进入工程打包构建的目录，构建
-cd ${build_dir}
+#cd ${build_dir}
 # npm install
 npm run build
  
