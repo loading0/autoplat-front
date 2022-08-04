80dd1db27395c1937d663685098d3b2e9e21d32c
loading0 <307958035@qq.com>
2022-08-04 15:29:24 +0800

    debug  yarn-build.sh script

--- yarn-build.sh
+++ yarn-build.sh
@@ -14,7 +14,7 @@ echo "删除以前构建的dist文件目录 >>> ${build_dir}/dist"
  
 # 进入工程打包构建的目录，构建
 cd ${build_dir}
-npm install
+# npm install
 npm run build
  
 echo -e "\n--------------- npm开始构建打包 end ---------------\n"
